d2b26ff5e2ca0c1b5d11f5ad4fa69eeb4de52f6c
Clockmerk <cblick@yandex.ru>
2023-02-27 21:05:42 +0300

    2.5 update

--- JS/script.js
+++ JS/script.js
@@ -12,12 +12,12 @@ const $cardsection = document.querySelector(".tea-section")
 
 const generateCard = (tea) => {
     return (`
-    <div class="tea-${tea.id} tea-div" style="width: 300px; margin: 20px;">
-        <img src="${tea.image}" class="card-img-top" alt="Фото чайника" style="max-height:280px; ">
+    <div class="tea-${tea.id} tea-div" id="tea${tea.id}" style="width: 300px; margin: 20px;">
+        <img src="${tea.image}" class="card-img-top" alt="Фото чайника">
         <div class="tea-card-body">
             <h5 class="tea-title">${tea.name}</h5>
             <p class="tea-text">${tea.description}</p>
-            <a href="#" class="btn btn-primary" style="background-color: yellow; color:black;">Подробнее</a>
+            <a href="#tea${tea.id}" class="btn btn-primary" style="background-color: yellow; color:black;">Подробнее</a>
     </div>
   </div>`)
 }
