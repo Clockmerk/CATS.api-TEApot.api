2ee7579f3594bb09713529575c45fe5fa30e21af
Clockmerk <cblick@yandex.ru>
2023-02-27 18:39:51 +0300

    Second stage

--- JS/api.js
+++ JS/api.js
@@ -1,1 +1,31 @@
-/*empty*/
+class apiInterface {
+    constructor(basename) {
+        this.url = `https://cats.petiteweb.dev/api/single/${basename}`
+    }
+    async getAll() {
+        const responce = await fetch(`${this.url}/show`);
+        const data = await responce.json();
+        return data;
+    }
+    async getIdTea(id) {
+        const responce = await fetch(`${this.url}/show/${id}`)
+        const data = await responce.json()
+        return data;
+    }
+    addTea(data) {
+        return fetch(`${this.url}/add`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    }
+    deleteTea(id) {
+        return fetch(`${this.url}/delete/${id}`, {
+            method: "DELETE",
+        })
+    }
+}
+const DBname = "Clockwerk"
+const teaApi = new apiInterface(DBname)
--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,30 @@
-/*empty*/
+/*const data = teaApi.getIdCat(3)
+data
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+*/
+const $cardsection = document.querySelector(".tea-section")
+
+
+const generateCard = (tea) => {
+    return (`
+    <div class="tea-${tea.id} tea-div" style="width: 300px; margin: 20px;">
+        <img src="${tea.image}" class="card-img-top" alt="Фото чайника" style="max-height:280px; ">
+        <div class="tea-card-body">
+            <h5 class="tea-title">${tea.name}</h5>
+            <p class="tea-text">${tea.description}</p>
+            <a href="#" class="btn btn-primary" style="background-color: yellow; color:black;">Подробнее</a>
+    </div>
+  </div>`)
+}
+const getTeas = async () => {
+    const res = await teaApi.getAll();
+    res.forEach(tea => {
+        $cardsection.insertAdjacentHTML('afterbegin', generateCard(tea))
+    });
+  }
+getTeas()
